56aa35b16abfe0876807d8f0363698d6eee8b2ba
Giovanni <giovanni.moreira@aluno.impacta.edu.br>
2026-03-09 21:46:37 -0300

    Resolve conflito de merge com a main

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,3 @@
 # Calculadora Básica
-<<<<<<< HEAD
-# Desenvolvedor A: Implementar a função de soma aqui
-def subtracao(a, b): return a - b 
-=======
 def soma(a, b): return a + b
-# Desenvolvedor B: Implementar a função de subtração aqui
->>>>>>> main
-
+def subtracao(a, b): return a - b 
